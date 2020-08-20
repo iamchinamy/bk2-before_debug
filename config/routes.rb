18ab2a068947f9ecb41262bb94ce3da2fcc6010e
Chinami Kitagawa <cnm25.k@gmail.com>
2020-08-20 17:07:05 +0900

    [finish]応用課題5

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
   root 'home#top'
   get 'home/about' => 'home#about'
+
   devise_for :users
+
   resources :users, only: [:show, :index, :edit, :update] do
 	  resource :relationships, only: [:create, :destroy]
     get :followings, on: :member
     get :followers, on: :member
   end
+  
   resources :books do
   	resources :book_comments, only: [:create, :destroy]
   	resource :favorites, only: [:create, :destroy]
   end
+
+  get 'search' => 'searches#search'
+
 end
